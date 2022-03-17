14851ffed47825779b3047b691bdf2e6d841727b
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-03-17 23:39:41 +0500

    Refactor using lombok annotations

--- src/main/java/example.java
+++ src/main/java/example.java
@@ -1,16 +1,11 @@
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 class example {
-    @Getter
-    @Setter
     String name = "John";
-    @Getter
-    @Setter
     String secondName = "Somebody";
-
-    @Override
-    public String toString() {
-        return "Hi, " + name +' '+secondName+'!';
-    }
 }
