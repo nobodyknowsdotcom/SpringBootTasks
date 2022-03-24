def017713cd0c8ef518287e0e6d17cfe58d68ffb
nobodyzhere <myclosestisa@gmail.com>
2022-03-24 12:14:33 +0500

    Fix double MyApplication init

--- src/main/java/com/example/demo/MyApplication.java
+++ src/main/java/com/example/demo/MyApplication.java
@@ -1,22 +1,21 @@
 package com.example.demo;
 
-import com.example.demo.config.Config;
 import com.example.demo.vehicles.Car;
 import com.example.demo.vehicles.CarWithFieldInjection;
+import com.example.demo.vehicles.CarWithSetterInjection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
-public class MyApplication {
+public class MyApplication{
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
-        ApplicationContext javaConfigContext = new AnnotationConfigApplicationContext(Config.class);
+        var applicationContext = SpringApplication.run(MyApplication.class, args);
+        CarWithSetterInjection setterCar = applicationContext.getBean(CarWithSetterInjection.class);
+        Car constructorCar = applicationContext.getBean(Car.class);
+        CarWithFieldInjection fieldCar = applicationContext.getBean(CarWithFieldInjection.class);
 
-        Car constructorCar = javaConfigContext.getBean(Car.class);
-        CarWithFieldInjection fieldCar = javaConfigContext.getBean(CarWithFieldInjection.class);
-        constructorCar.getWheel().Roll();
-        constructorCar.getSteeringWheel().Roll();
+        setterCar.Work();
+        constructorCar.Work();
+        fieldCar.Work();
     }
 }
