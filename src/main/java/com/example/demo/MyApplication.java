6204496303d5af8f7f0fbd05ef9bc1f8d4c5b4b4
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-04-05 20:17:52 +0500

    Add try/catch blocks

--- src/main/java/com/example/demo/MyApplication.java
+++ src/main/java/com/example/demo/MyApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import com.example.demo.config.ConfigInfo;
 import com.example.demo.vehicles.Car;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyApplication{
-
     public static void main(String[] args) {
         var applicationContext = SpringApplication.run(MyApplication.class, args);
         Car myCar = applicationContext.getBean(Car.class);
@@ -15,7 +15,19 @@ public class MyApplication{
         System.out.println(config);
         System.out.printf("Wheels size is %s\n", myCar.getWheelsSize());
         System.out.printf("Steering wheel size is %s\n", myCar.getSteeringWheelSize());
-        System.out.println(applicationContext.getBean("isNotDefault"));
         myCar.work();
+
+        try {
+            System.out.println(applicationContext.getBean("testString"));
+            System.out.println(applicationContext.getBean("secondTestString"));
+        } catch (NoSuchBeanDefinitionException e) {
+            System.out.println("Test Bean not found!");
+        }
+
+        try {
+            System.out.println(applicationContext.getBean("isNotDefault"));;
+        } catch (NoSuchBeanDefinitionException e) {
+            System.out.println("This is default config.");
+        }
     }
 }
