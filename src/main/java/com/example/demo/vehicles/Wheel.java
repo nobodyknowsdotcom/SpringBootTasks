fcdeec38ea6b3e0b396250ab40b6fae573ee4c0a
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-04-05 21:39:45 +0500

    Remove unusual @Component annotation

--- src/main/java/com/example/demo/vehicles/Wheel.java
+++ src/main/java/com/example/demo/vehicles/Wheel.java
@@ -1,9 +1,7 @@
 package com.example.demo.vehicles;
 
 import lombok.Getter;
-import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 public class Wheel extends Rollable{
     public Wheel(int size){
