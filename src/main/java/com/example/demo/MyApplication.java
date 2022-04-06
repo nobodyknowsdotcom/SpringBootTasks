7a06fd72fcacea45944002f8ea0ed58294d71ffe
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-04-06 21:36:46 +0500

    Get application context via constructor

--- src/main/java/com/example/demo/MyApplication.java
+++ src/main/java/com/example/demo/MyApplication.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
-import com.example.demo.config.ConfigInfo;
 import com.example.demo.vehicles.Car;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyApplication{
+    private static ApplicationContext applicationContext;
+
+    public MyApplication(ApplicationContext applicationContext) {
+        MyApplication.applicationContext = applicationContext;
+    }
+
     public static void main(String[] args) {
-        var applicationContext = SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(MyApplication.class, args);
         Car myCar = applicationContext.getBean(Car.class);
         System.out.printf("Wheels size is %s\n", myCar.getWheelsSize());
         System.out.printf("Steering wheel size is %s\n", myCar.getSteeringWheelSize());
@@ -23,7 +29,7 @@ public class MyApplication{
         }
 
         try {
-            System.out.println(applicationContext.getBean("isNotDefault"));;
+            System.out.println(applicationContext.getBean("isNotDefault"));
         } catch (NoSuchBeanDefinitionException e) {
             System.out.println("This is default config.");
         }
