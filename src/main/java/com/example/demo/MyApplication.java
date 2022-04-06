92f4f151ada53660f9cbc3f546ddf78e06e1c7f8
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-04-06 21:36:46 +0500

    Get Car bean via constructor

--- src/main/java/com/example/demo/MyApplication.java
+++ src/main/java/com/example/demo/MyApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.config.ConfigInfo;
 import com.example.demo.vehicles.Car;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyApplication{
+    private static Car myCar;
+
+    public MyApplication(Car myCar) {
+        MyApplication.myCar = myCar;
+    }
+
     public static void main(String[] args) {
         var applicationContext = SpringApplication.run(MyApplication.class, args);
-        Car myCar = applicationContext.getBean(Car.class);
         System.out.printf("Wheels size is %s\n", myCar.getWheelsSize());
         System.out.printf("Steering wheel size is %s\n", myCar.getSteeringWheelSize());
         myCar.work();
@@ -23,7 +27,7 @@ public class MyApplication{
         }
 
         try {
-            System.out.println(applicationContext.getBean("isNotDefault"));;
+            System.out.println(applicationContext.getBean("isNotDefault"));
         } catch (NoSuchBeanDefinitionException e) {
             System.out.println("This is default config.");
         }
