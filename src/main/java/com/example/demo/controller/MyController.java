c3d22f3ffd618e3aed3315b362057d0368b8554b
Dmitry Medvedev <myclosestisa@gmail.com>
2022-04-16 13:53:53 +0500

    Read nested JSON elements

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,12 +1,10 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.MyInput;
-import com.example.demo.dto.MyOutput;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,19 +13,14 @@ import java.util.Map;
 @Slf4j
 @Controller
 public class MyController {
-    @GetMapping("/greeting/{name}")
-    public String myGreeting(Model model,@PathVariable String name) {
-        model.addAttribute("user.name", name);
-        return "greeting";
+    @GetMapping("/addid")
+    public ResponseEntity<String> myGreeting(@RequestBody JsonNode input) {
+        return new ResponseEntity<>(input.findValues("date").toString(), HttpStatus.OK);
     }
     @GetMapping("/getheaders")
     public ResponseEntity<String> listAllHeaders(@RequestHeader Map<String, String> headers) {
         var headersList= new ArrayList<String>();
         headers.forEach((key, value) -> headersList.add(String.format("Header '%s' = %s", key, value)));
         return new ResponseEntity<>(String.join("\n", headersList), HttpStatus.OK);
-    }
-    @PostMapping("/greeting")
-    public ResponseEntity<MyOutput> myPostGreeting(@RequestBody MyInput input) {
-        return new ResponseEntity<>(new MyOutput(input.getGreet() + " " + input.getName() + "!"), HttpStatus.OK);
     }
 }
