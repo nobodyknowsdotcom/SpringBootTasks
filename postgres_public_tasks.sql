826b1168028784e4cdde2f2744e6393c264aa683
Dmitry Medvedev <myclosestisa@gmail.com>
2022-04-28 00:46:34 +0500

    fix database port

--- postgres_public_tasks.sql
+++
@@ -1,4 +0,0 @@
-UPDATE public.tasks SET name = 'My tasks' WHERE id = 19;
-UPDATE public.tasks SET name = 'Мой список дел' WHERE id = 20;
-UPDATE public.tasks SET name = 'Мой список дел' WHERE id = 21;
-UPDATE public.tasks SET name = 'my super tasks' WHERE id = 22;
